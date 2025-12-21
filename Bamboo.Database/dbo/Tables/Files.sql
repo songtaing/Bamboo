53f8dff5bb60336df6fd3e5d82aca6926983b469
songtaing <songtaing@gmail.com>
2025-12-21 13:46:24 -0800

    Upgraded to dotnet 10

---
+++ Bamboo.Database/dbo/Tables/Files.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Files] (
+    [Id]     INT IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR(MAX) NOT NULL,
+    [RefCode] UNIQUEIDENTIFIER NOT NULL,
+    [CreateDate] DATETIME     NOT NULL,
+    [CreateBy] INT NOT NULL,
+    [IsActive] BIT NOT NULL,
+    [FolderId]   INT          NOT NULL,
+    CONSTRAINT [PK_Files] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Bamboo.Database/dbo/Tables/Folders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Folders] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR(MAX) NOT NULL,
+    [RefCode] UNIQUEIDENTIFIER NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [CreateBy] INT NOT NULL,
+    [IsActive] BIT NOT NULL,
+    CONSTRAINT [PK_Folders] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
